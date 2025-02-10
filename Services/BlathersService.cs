8d3d07eba0c4fac51fdec1d4dc5453e78cbff8c9
rochellew <rochellew@etsu.edu>
2025-02-10 00:26:55 -0500

    moved the files

--- Services/BlathersService.cs
+++
@@ -1,7 +0,0 @@
-public class BlathersService : IBlathersService
-{
-    private string[] facts = { "Tarantulas are actually quite shy!", "Oarfish can grow up to 26 feet!", "Scorpions grow under UV light!" };
-    private Random rnd = new Random();
-
-    public string GetRandomFact() => facts[rnd.Next(facts.Length)];
-}
